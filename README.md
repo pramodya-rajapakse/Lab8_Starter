c92bbdb76de1e81d451b4d43ada7fb0ce01dc35c
pramodya-rajapakse <102259936+pramodya-rajapakse@users.noreply.github.com>
2023-05-28 17:38:02 -0700

    completed expose and explore

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Lab 8 - Starter
+Pramodya Rajapakse
+
+1. The automated tests should be in a Github action because that way they can be run automatically, without needing to spend time running it locally on the developer's end, and it can be at every push, so the code can be tested at every stage of development from the beginning. This will allow you to catch errors and changes you want to make early in development.
+2. No, testing if a function is returning the correct output is more a unit test.
+3. No, you would not use unit testing for this, at it involves checking multiple part of your code. You have to check if the message is written up properly, sent accordingly, and then recieved by the other user, so this is more like end-to-end testing.
+4. Yes, you would use unit testing for this feature, since it involves testing a single part of your code. You only need to check whether a given message is sent, depending on its length, which you can test in isolation without involving other parts of your code.  
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -1,5 +1,90 @@
 // unit.test.js
 
-const functions = require('../code-to-unit-test/unit-test-me.js');
+const functions = require("../code-to-unit-test/unit-test-me.js");
 
 // TODO - Part 2
+
+// isPhoneNumberTests
+test("testing '(123)-456-7890' to be valid phone number", () => {
+  expect(functions.isPhoneNumber("(123)-456-7890")).toBe(true);
+});
+
+test("testing '123-456-7890' to be valid phone number", () => {
+  expect(functions.isPhoneNumber("(123)-456-7890")).toBe(true);
+});
+
+test("testing '(123)-456' to be invalid phone number", () => {
+  expect(functions.isPhoneNumber("(123)-456")).toBe(false);
+});
+
+test("testing '(123)-456-78901' to be invalid phone number", () => {
+  expect(functions.isPhoneNumber("(123)")).toBe(false);
+});
+
+// isEmail tests
+test("testing 'johndoe@gmail.com' to be valid email", () => {
+  expect(functions.isEmail("johndoe@gmail.com")).toBe(true);
+});
+
+test("testing 'johndoegmail.com' to be invalid email", () => {
+  expect(functions.isEmail("johndoegmail.com")).toBe(false);
+});
+
+test("testing 'somebody123@hotmail.com' to be valid email", () => {
+  expect(functions.isEmail("somebody123@hotmail.com")).toBe(true);
+});
+
+test("testing 'somebody123@hotmail' to be invalid email", () => {
+  expect(functions.isEmail("somebody123@hotmail")).toBe(false);
+});
+
+// isStrongPassword tests
+test("testing 'something_23' to be valid password", () => {
+  expect(functions.isStrongPassword("something_23")).toBe(true);
+});
+
+test("testing 'password' to be valid password", () => {
+  expect(functions.isStrongPassword("password")).toBe(true);
+});
+
+test("testing '!$%thing' to be invalid password", () => {
+  expect(functions.isStrongPassword("!$%thing")).toBe(false);
+});
+
+test("testing 'rye' to be invalid password", () => {
+  expect(functions.isStrongPassword("rye")).toBe(false);
+});
+
+// isDate tests
+test("testing '1/2/2023' to be valid date", () => {
+  expect(functions.isDate("1/2/2023")).toBe(true);
+});
+
+test("testing '01/02/2023' to be valid date", () => {
+  expect(functions.isDate("1/2/2023")).toBe(true);
+});
+
+test("testing '1/2/23' to be invalid date", () => {
+  expect(functions.isDate("1/2/23")).toBe(false);
+});
+
+test("testing 'january' to be invalid date", () => {
+  expect(functions.isDate("january")).toBe(false);
+});
+
+// isHexColor tests
+test("testing '#72473E' to be valid hex code", () => {
+  expect(functions.isHexColor("#72473E")).toBe(true);
+});
+
+test("testing '#f0c' to be valid hex code", () => {
+  expect(functions.isHexColor("#f0c")).toBe(true);
+});
+
+test("testing '#7247' to be invalid hex code", () => {
+  expect(functions.isHexColor("#7247")).toBe(false);
+});
+
+test("testing '#%64*yf' to be invalid hex code", () => {
+  expect(functions.isHexColor("#%64*yf")).toBe(false);
+});
